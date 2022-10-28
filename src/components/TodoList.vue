6b7af8b184d1d4f77e209a878f3461967f93e577
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-10-28 16:11:28 +0200

    basic layout

---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { NInput, NButton } from "naive-ui";
+const task = ref("");
+</script>
+
+<template>
+  <div class="flex place-content-center mt-6">
+    <div class="flex w-72">
+      <NInput v-model:value="task" placeholder="Write your task here"> </NInput>
+      <n-button size="small">Add task </n-button>
+    </div>
+  </div>
+  <h1>{{ task }}dsa</h1>
+</template>
